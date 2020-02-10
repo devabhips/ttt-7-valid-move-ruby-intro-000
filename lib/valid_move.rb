bdc68659a44631cdd4d870fc2ea3179d052ff1d9
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-10 18:44:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 # code your #valid_move? method here
 def valid_move?(board, index)
-  return board[index].nil? || board[index] == ' ' || board[index] == ''
-  return true if board[index] == ' X ' || board[index] == ' O '
-  return nil
+  return !board[index].nil? || board[index] == ' ' || board[index] == '' || board[index] == ' X ' || board[index] == ' O '
+
+  return true if 
+
+  nil
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
